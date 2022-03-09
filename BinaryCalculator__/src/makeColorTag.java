5fc7324591b44de12998ee5f4f94ba4093652d2c
Sami Hindi <lolipop66611@gmail.com>
2022-03-09 19:06:55 +0100

    Fix: To Hexadecimal will now add a 0 in front of Hex if Hex length is equal to 1

--- BinaryCalculator__/src/makeColorTag.java
+++ BinaryCalculator__/src/makeColorTag.java
@@ -7,13 +7,29 @@ public class makeColorTag {
         int redDec;
         int greenDec;
         int blueDec;
+
         redDec = Integer.parseInt(teile[0]);
         greenDec = Integer.parseInt(teile[1]);
         blueDec = Integer.parseInt(teile[2]);
+        String redDecHex = Integer.toHexString(redDec);
+        // Check
+        if(redDecHex.length() == 1) {
+            redDecHex = "0" + (redDecHex);
+        }
+        String greenDecHex = Integer.toHexString(greenDec);
+        // Check
+        if(greenDecHex.length() == 1) {
+            greenDecHex = "0" + (greenDecHex);
+        }
+        String blueDecHex = Integer.toHexString(blueDec);
+        // Check
+        if(blueDecHex.length() == 1 ) {
+            blueDecHex = "0" + (blueDecHex);
+        }
         String formula = "";
-        formula += Integer.toHexString(redDec);
-        formula += Integer.toHexString(greenDec);
-        formula += Integer.toHexString(blueDec);
+        formula += redDecHex;
+        formula += greenDecHex;
+        formula += blueDecHex;
         System.out.println("#"+formula);
     }
 
